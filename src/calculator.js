ceda1bfadd5714537d8aad41921b83b1e01f270f
Hanin <hanin.barkawe@gmail.com>
2024-05-08 11:17:25 +0200

    still failing :((

--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,11 @@
 class Calculator {
     add(a, b) {
-       return Number(a) + Number(b);
+        return Number(a) + Number(b);
     }
- }
- 
+
+    subtract(a, b) {
+        return Number(a) - Number(b);
+    }
+}
 
 module.exports = Calculator;
- 
--- tests/calculator2.test.js
+++ tests/calculator2.test.js
@@ -1,22 +1,24 @@
-const { Builder, By } = require('selenium-webdriver');
-require('chromedriver');
+const { Builder, By, until } = require('selenium-webdriver');
+require('geckodriver');
 
-async function testCalculatorAddition() {
-    let driver = await new Builder().forBrowser('chrome').build();
-    try {
-        // Change to the URL where your calculator app is hosted, e.g., 'http://localhost:3000'
-        await driver.get('file:///Users/haninbarkawe/testing-with-jest/src/calculator.html');
-        await driver.findElement(By.id('num1')).click();
-        await driver.findElement(By.id('add')).click();
-        await driver.findElement(By.id('num2')).click();
-        await driver.findElement(By.id('equals')).click();
-        let result = await driver.findElement(By.id('display')).getText();
-        if (result !== '4') {  // This should fail as 1 + 2 = 3
-            throw new Error('Assertion failed: Expected display to be 4');
-        }
-    } finally {
-        await driver.quit();
-    }
-}
+const fileUnderTest = 'file://' + __dirname.replace(/ /g, '%20') + '/../src/calculator.html';
+const defaultTimeout = 10000;
+let driver;
 
-testCalculatorAddition().catch(err => console.error(err));
+beforeAll(async () => {
+    driver = await new Builder().forBrowser('firefox').build();
+    await driver.get(fileUnderTest);
+});
+
+afterAll(async () => {
+    await driver.quit();
+}, defaultTimeout);
+
+test('should subtract two numbers correctly', async () => {
+    await driver.findElement(By.id('num2')).click(); // Click '2'
+    await driver.findElement(By.id('subtract')).click(); // Click '-'
+    await driver.findElement(By.id('num1')).click(); // Click '1'
+    await driver.findElement(By.id('equals')).click(); // Click '='
+    let result = await driver.findElement(By.id('display')).getText();
+    expect(result).toEqual('1'); // Check if the display shows '1' after subtracting 1 from 2
+});
